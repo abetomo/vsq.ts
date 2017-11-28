df37bba4d52947bb48458ae1baf79f5b94142ced
abetomo <abe@enzou.tokyo>
2017-11-28 18:58:59 +0900

    Add setting to lint by standard

--- preprocessor.js
+++ preprocessor.js
@@ -1,14 +1,14 @@
 // Copyright 2004-present Facebook. All Rights Reserved.
 // https://github.com/facebook/jest/blob/master/examples/typescript/preprocessor.js
 
-const tsc = require('typescript');
-const tsConfig = require('./tsconfig.json');
+const tsc = require('typescript')
+const tsConfig = require('./tsconfig.json')
 
 module.exports = {
-  process(src, path) {
+  process (src, path) {
     if (path.endsWith('.ts') || path.endsWith('.tsx')) {
-      return tsc.transpile(src, tsConfig.compilerOptions, path, []);
+      return tsc.transpile(src, tsConfig.compilerOptions, path, [])
     }
-    return src;
-  },
-};
+    return src
+  }
+}
